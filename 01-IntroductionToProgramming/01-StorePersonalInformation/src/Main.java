c2ad0995a194ad2ef76ed800e4b88aa82724c167
Sajadi <69210109+SMSajadi99@users.noreply.github.com>
2021-09-01 21:38:08 +0430

    Add files via upload

---
+++ 01-IntroductionToProgramming/01-StorePersonalInformation/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "SeyedMohammadSajadi";
+        int age = 22;
+        char sex = 'M';
+        float grade = 18.49f;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(sex);
+        System.out.println(grade);
+    }
+}
---
+++ 01-IntroductionToProgramming/02-Computing/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 12;
+        float c = 2.67f;
+
+        int result_sum = a+b;
+        float result_mux = a*c;
+        float result_div = c/b;
+        float result_Sub = b - c;
+
+        System.out.println(result_sum);
+        System.out.println(result_mux);
+        System.out.println(result_div);
+        System.out.println(result_Sub);
+    }
+}
