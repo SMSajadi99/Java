c4442ca457df288f382a2cecc0f1af58d62227a5
Sajadi <69210109+SMSajadi99@users.noreply.github.com>
2021-09-04 02:12:08 +0430

    Add files via upload

---
+++ 02-ObjectOrientedProgramming(OOP)/03-inheritance/src/Google.java
@@ -0,0 +1,11 @@
+public class Google extends Company {
+    private int number;
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
